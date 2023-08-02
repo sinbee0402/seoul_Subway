7567c22c172ad0bc4e360c7e3814021447139fa7
sinbee0402 <osb3476@gmail.com>
2023-08-02 16:53:45 +0900

    Feat: provider 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:seoul_subway/data/repository/subway_station_repository_impl.dart';
+import 'package:seoul_subway/domain/use_case/get_station_use_case.dart';
 import 'package:seoul_subway/presentation/main/main_screen.dart';
+import 'package:seoul_subway/presentation/main/main_view_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MainViewModel(
+            GetStationUseCase(SubwayStationRepositoryImpl()),
+          ),
+        )
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
